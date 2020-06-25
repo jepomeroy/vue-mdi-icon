3180c5260e4c2c4138e29b95d7a4ed04de2d430e
Earl Pomeroy <earl.pomeroy42@gmail.com>
2020-06-24 23:00:14 -0600

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,30 @@
+<script>
+import Vue from 'vue'
+import { mdiHome } from '@mdi/js'
+import VueMdiIcon from '@/vue-mdi-icon.vue'
+
+export default Vue.extend({
+    name: 'ServeDev',
+    components: {
+        VueMdiIcon,
+    },
+    data() {
+        return {
+            icon: mdiHome,
+        }
+    },
+})
+</script>
+
+<template>
+    <div id="app">
+        <vue-mdi-icon
+            :path="icon"
+            size="60"
+            rotate="-12.33"
+            title="Home"
+            description="Home Icon"
+            color="#24a39b"
+        />
+    </div>
+</template>
---
+++ src/vue-mdi-icon.vue
@@ -0,0 +1,69 @@
+<template>
+    <svg :width="`${iconSize()}`" :height="`${iconSize()}`" viewBox="0 0 24 24">
+        <template v-if="title">
+            <title>{{ title }}</title>
+        </template>
+        <template v-if="description">
+            <description>{{ description }}</description>
+        </template>
+        <g :fill="color" :transform="rotation">
+            <path :d="icon" />
+        </g>
+    </svg>
+</template>
+<script>
+export default {
+    name: 'icon',
+    computed: {
+        viewBox() {
+            return `0 0 ${this.iconSize()} ${this.iconSize()}`
+        },
+        rotation() {
+            return `rotate(${this.rotate} 12 12)`
+        },
+    },
+    data() {
+        return {
+            icon: this.path,
+        }
+    },
+    methods: {
+        iconSize() {
+            if (this.isNumeric(this.size)) {
+                return `${this.size}px`
+            }
+
+            return this.size
+        },
+        isNumeric(value) {
+            return /^-{0,1}\d+$/.test(value)
+        },
+    },
+    props: {
+        path: {
+            type: String,
+            required: true,
+        },
+        title: String,
+        description: String,
+        size: {
+            type: [Number, String],
+            default: 24,
+        },
+        horizontal: Boolean,
+        vertical: Boolean,
+        rotate: {
+            type: [Number, String],
+            default: 0,
+        },
+        color: {
+            type: String,
+            default: '#fff',
+        },
+        spin: {
+            type: Boolean || String,
+            default: false,
+        },
+    },
+}
+</script>

12647313e8ec8f1ed6cbc063620b5ed4fcaaccd5
Earl Pomeroy <epomeroy@ultramain.com>
2020-06-27 09:34:01 -0600

    Removed data and just used path prop

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,6 +1,37 @@
+<template>
+    <div id="app">
+        <h4 class="title">{{ `Showing ${state ? 'Home' : 'Airplane'} Icon` }}</h4>
+        <div class="icon">
+            <template v-if="this.state">
+                <vue-mdi-icon
+                    :path="mdiHome"
+                    size="60"
+                    rotate="-12.33"
+                    title="Home"
+                    description="Home Icon"
+                    color="#24a39b"
+                />
+            </template>
+            <template v-else>
+                <vue-mdi-icon
+                    :path="mdiAirplane"
+                    size="60"
+                    rotate="90"
+                    title="Aiplane"
+                    description="Aiplane Icon"
+                    color="#ffa39b"
+                />
+            </template>
+        </div>
+        <div class="input">
+            <input type="button" @click="switchIcons" value="Switch Icons" />
+        </div>
+    </div>
+</template>
+
 <script>
 import Vue from 'vue'
-import { mdiHome } from '@mdi/js'
+import { mdiHome, mdiAirplane } from '@mdi/js'
 import VueMdiIcon from '@/vue-mdi-icon.vue'
 
 export default Vue.extend({
@@ -10,21 +41,15 @@ export default Vue.extend({
     },
     data() {
         return {
-            icon: mdiHome,
+            mdiHome,
+            mdiAirplane,
+            state: false,
         }
+    },
+    methods: {
+        switchIcons() {
+            this.state = !this.state
+        },
     },
 })
 </script>
-
-<template>
-    <div id="app">
-        <vue-mdi-icon
-            :path="icon"
-            size="60"
-            rotate="-12.33"
-            title="Home"
-            description="Home Icon"
-            color="#24a39b"
-        />
-    </div>
-</template>
